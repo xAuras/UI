e5092d2725b577b4d5eedcee9d3f97b821d05634
xAuras <67531498+xAuras@users.noreply.github.com>
2023-02-22 15:59:43 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1876,7 +1876,6 @@ function Bracket:Notification(Notification)
     Notification.Description = GetType(Notification.Description,"Description","string")
 
     local NotificationAsset = GetAsset("Notification/ND")
-    print(NotificationAsset.ClassName)
     NotificationAsset.Parent = Bracket.ScreenAsset.NDHandle
     NotificationAsset.Title.Text = Notification.Title
     NotificationAsset.Description.Text = Notification.Description
